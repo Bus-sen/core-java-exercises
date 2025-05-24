80458371fbefea349c1be0c8602d62d6d5675715
Bus-sen <snbusra66@gmail.com>
2025-05-24 13:39:08 +0300

    [JA-9] - while döngüsü kullanıldı

---
+++ src/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int number = 10;
+        while (1 <= number) {
+            System.out.println(number);
+            number--;
+
+        }
+    }
+}
