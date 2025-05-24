bab4d3e310a3206dd60724a9dababc2aa8f9d803
Bus-sen <snbusra66@gmail.com>
2025-05-24 12:00:12 +0300

    [JA-2] - veri tipleriyle değişkenler tanımlandı

---
+++ src/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int number = 66;
+        System.out.println(number);
+
+        double number2 = 66.6;
+        System.out.println(number2);
+
+        float number3 = 66.66f;
+        System.out.println(number3);
+
+        boolean bool = false;
+        System.out.println(bool);
+
+        char b = 'b';
+        System.out.println(b);
+
+        String name = "Busra";
+        System.out.println(name);
+
+        final int finalNumber = 25;
+        System.out.println(finalNumber);
+    }
+}
